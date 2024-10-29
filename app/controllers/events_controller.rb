ed59dca7d8c8288e98390528e7d4a29c2b331e7b
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-10-29 14:09:13 -0300

    feat: add #new & #create actions to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,26 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [ :new, :create ]
+
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      redirect_to user_path(current_user.id), notice: "Event created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def event_params
+      params.require(:event).permit(:title, :body, :date, :location)
+    end
 end
