533b890f122bf6bcc1d3373ec9ac98f673cf9fea
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-10-29 12:13:39 -0300

    chore(db): migrate the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_29_143439) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_29_150243) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_29_143439) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +34,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_29_143439) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "events", "users", column: "creator_id"
 end
