a2f5d7ce32930dd2450cbdea5099b3077fa1b812
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-10-29 11:40:49 -0300

    feat: create user model & migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_29_133245) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_29_143439) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -18,5 +18,18 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_29_133245) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
