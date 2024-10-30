6732c1f802189ee4db580710bee72c7882611e58
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-10-30 16:36:30 -0300

    feat: display attended events on user's #show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
 
   def show
     @user_created_events = current_user.created_events
+    @user_attended_events = current_user.attended_events
   end
 end
