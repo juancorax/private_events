cd49f2824adb3ba2bb9a119aa4c4081af3c4b856
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-10-30 09:31:02 -0300

    feat: add associations between attendee & event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
+  has_many :attendances, foreign_key: :attended_event_id
+  has_many :attendees, through: :attendances, source: :attendee
+
   belongs_to :creator, class_name: "User"
 end
