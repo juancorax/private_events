15649b7ac20dee1e190e15e5d04224f4e453be61
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-10-31 13:50:09 -0300

    feat: create class methods to separate past & upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ApplicationRecord
+  def self.past
+    where("date < ?", DateTime.current)
+  end
+
+  def self.upcoming
+    where("date >= ?", DateTime.current)
+  end
+
   has_many :attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :attendances, source: :attendee
 
